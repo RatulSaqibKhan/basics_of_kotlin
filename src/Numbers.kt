06672b43331132e0987b3ce336b97050fa2113d6
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2020-08-28 23:39:56 +0600

    Basic math operators

---
+++ src/Numbers.kt
@@ -0,0 +1,13 @@
+package demo
+
+fun main(args: Array<String>) {
+    val a = 14
+    val b = 45
+
+    // + - * / %
+    println(a + b)
+    println(b - a)
+    println(b * a)
+    println(b / a)
+    println(b % a)
+}
